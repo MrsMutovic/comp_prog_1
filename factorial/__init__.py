49a7721beb93d26ed90bf8003c7fda5b298e80c3
MrsMutovic <30479622+MrsMutovic@users.noreply.github.com>
2019-11-06 11:59:34 -0500

    Update __init__.py

--- factorial/__init__.py
+++ factorial/__init__.py
@@ -1,23 +1,37 @@
 import check50
+import check50.py
+import os
 
 @check50.check()
 def exists():
     """factorial.py exists"""
     check50.exists("factorial.py")
-
 @check50.check(exists)
+
+
 
 def rejectneg():
     """rejects negative numbers"""
-    from re import match
-    expected = "Please enter a non-negative number: "
-    actual = check50.run("python3 factorial.py").stdin("-5", prompt=False).stdout()
-    if not match(expected, actual):
-        help = None
-        raise check50.Mispatch("factorial\n", actual)
-def factorial():
-    """factorial"""
-    check50.run("python3 factorial.py").stdin("0", prompt=False).stdout("1", regex=False).exit(0)
-    check50.run("python3 factorial.py").stdin("1", prompt=False).stdout("1", regex=False).exit(0)
-    check50.run("python3 factorial.py").stdin("5", prompt=False).stdout("120", regex=False).exit()
-    check50.run("python3 factorial.py").stdin("15", prompt=False).stdout("1307674368000", regex=False).exit()
+    check50.run("python3 factorial.py").stdin("-5").reject()
+@check50.check(exists)
+
+
+def factorial0():
+    """factorial for 0"""
+    check50.run("python3 factorial.py").stdin("0").stdout("1").exit(0)
+@check50.check(exists)
+
+def factorial1():
+    """factorial for 1"""
+    check50.run("python3 factorial.py").stdin("1").stdout("1").exit(0)
+@check50.check(exists)
+
+def factoroal5();
+     """factorial for 5"""
+    check50.run("python3 factorial.py").stdin("5").stdout("120").exit(0)
+@check50.check(exists)
+
+def factorial15():
+     """factorial for 15"""
+    check50.run("python3 factorial.py").stdin("15").stdout("1307674368000").exit(0)
+@check50.check(exists)
